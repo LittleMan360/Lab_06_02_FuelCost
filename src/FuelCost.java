7cc4a94820aa61da721ea2fe08b590b85b15ee69
Dalton <dalton.m.towe@gmail.com>
2023-02-15 12:35:32 -0500

    Started code put variables

---
+++ src/FuelCost.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FuelCost {
+    public static void main(String[] args)
+    {
+
+
+        //variables
+        int gallonsPerTank;
+        int mPG;
+        double gasPerGallon;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter how many gallons you have in a tank ");
+        in.hasNextInt();
+
+
+
+    }
+}
